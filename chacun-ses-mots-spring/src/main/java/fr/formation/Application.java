7ef7c5168d5cca4ca2cda1c92b82b5127e27948d
helene <helene.yu@icd.igsgroupe.com>
2019-04-29 15:14:16 +0200

    les configs

--- chacun-ses-mots-spring/src/main/java/fr/formation/Application.java
+++ chacun-ses-mots-spring/src/main/java/fr/formation/Application.java
@@ -1,5 +1,23 @@
 package fr.formation;
 
-public class Application {
+import org.springframework.beans.factory.annotation.Autowired;
 
+import fr.formation.projet.IDAOJoueur;
+import fr.formation.projet.IDAOPartie;
+import fr.formation.projet.IDAOQuestion;
+import fr.formation.projet.IDAOReponse;
+
+public class Application {
+	@Autowired
+	private IDAOQuestion daoQuestion;
+	@Autowired
+	private IDAOReponse daoReponse;
+	@Autowired
+	private IDAOJoueur daoJoueur;
+	@Autowired
+	private IDAOPartie daoPartie;
+
+	public void run(String[] args) {
+
+	}
 }
--- chacun-ses-mots-spring/src/main/java/fr/formation/config/JpaConfig.java
+++ chacun-ses-mots-spring/src/main/java/fr/formation/config/JpaConfig.java
@@ -1,5 +1,84 @@
 package fr.formation.config;
 
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@PropertySource("classpath:data-source.properties")
+@EnableTransactionManagement
+@EnableJpaRepositories("fr.formation.dao")
 public class JpaConfig {
+	@Autowired
+	private Environment env;
+	
+	// Configuration acces BDD
+	@Bean // il est manageable par Spring
+	public BasicDataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+	
+	
+		// Propriete de connexion
+		dataSource.setDriverClassName(env.getProperty("sql.driver"));
+		dataSource.setUrl(env.getProperty("sql.url"));
+		dataSource.setUsername(env.getProperty("sql.username"));
+		dataSource.setPassword(env.getProperty("sql.password"));
+		dataSource.setMaxTotal(env.getProperty("sql.maxTotal",Integer.class));
+		
+		return dataSource;
+	}
+	
+
+
+
+	// Configuration EntityManagnerFactory
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(BasicDataSource dataSource) {
+		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		emf.setDataSource(dataSource);
+		emf.setPackagesToScan("fr.formation.model");
+		emf.setJpaVendorAdapter(vendorAdapter);
+		emf.setJpaProperties(this.hibernateProperties());
+		return emf;
+	}
+
+	private Properties hibernateProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+		properties.setProperty("hibernate.show_sql", "true");
+		properties.setProperty("hibernate.format_sql", "false");
+		return properties;
+	}
+
+	// COnfiguration Gestionnaire de transaction
+	@Bean
+public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+	JpaTransactionManager transactionManager = new JpaTransactionManager();
+	
+	transactionManager.setEntityManagerFactory(emf);
+	return transactionManager;
+}
+@Bean
+public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+	return new PersistenceExceptionTranslationPostProcessor();
+}
 
 }
