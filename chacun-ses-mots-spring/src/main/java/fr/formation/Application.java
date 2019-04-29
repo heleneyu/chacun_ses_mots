992d34fb8415fa6be56f652a5c65584460c6fb00
Sylvain Chea <sylvainmb@hotmail.com>
2019-04-29 16:14:26 +0200

    app maj

--- chacun-ses-mots-spring/src/main/java/fr/formation/Application.java
+++ chacun-ses-mots-spring/src/main/java/fr/formation/Application.java
@@ -1,8 +1,12 @@
 package fr.formation;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import fr.formation.model.Question;
+import fr.formation.model.Reponse;
 import fr.formation.projet.IDAOJoueur;
 import fr.formation.projet.IDAOPartie;
 import fr.formation.projet.IDAOQuestion;
@@ -22,5 +26,26 @@ public class Application {
 		for (Question q : daoQuestion.findAll()) {
 			System.out.println(q.getDonnee());
 		}
+	}
+	
+	public static ArrayList<Question> melangerPaquetQuestion(IDAOQuestion daoQuestion) {
+		// Creation du paquet de questions
+		ArrayList<Question> paquetQuestion = new ArrayList<Question>();
+		for (Question c : daoQuestion.findAll()) {
+			paquetQuestion.add(c);
+			c.setNbInput();
+		}
+		Collections.shuffle(paquetQuestion);
+		return paquetQuestion;
+	}
+
+	public static ArrayList<Reponse> melangerPaquetProposition(IDAOReponse daoReponse) {
+		// creation du paquet de proposition
+		ArrayList<Reponse> paquetReponse = new ArrayList<Reponse>();
+		for (Reponse c : daoReponse.findAll()) {
+			paquetReponse.add(c);
+		}
+		Collections.shuffle(paquetReponse);
+		return paquetReponse;
 	}
 }
