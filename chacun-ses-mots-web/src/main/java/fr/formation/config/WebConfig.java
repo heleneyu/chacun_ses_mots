c61d134e1cbf4eefb83c02f848f221bb4b310f0d
helene <helene.yu@icd.igsgroupe.com>
2019-05-03 11:42:54 +0200

    thymeleaff

---
+++ chacun-ses-mots-web/src/main/java/fr/formation/config/WebConfig.java
@@ -0,0 +1,54 @@
+package fr.formation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("fr.formation")
+public class WebConfig implements WebMvcConfigurer {
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+		registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+
+	}
+
+	@Bean
+	public UrlBasedViewResolver viewResolver() {
+		UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/templates/");
+		viewResolver.setSuffix(".html");
+		return viewResolver;
+	}
+	
+	@Bean
+	public SpringResourceTemplateResolver templateResolver() {
+		SpringResourceTemplateResolver templateResolver=new SpringResourceTemplateResolver();
+		templateResolver.setPrefix("/WEB-INF/templates/");
+	templateResolver.setSuffix(".html");
+	return templateResolver;}
+	
+	@Bean
+	public ThymeleafViewResolver viewResolver(SpringTemplateEngine templateEngine) {
+		ThymeleafViewResolver viewResolver=new ThymeleafViewResolver();
+	viewResolver.setTemplateEngine(templateEngine);
+	return viewResolver;}
+	
+	@Bean
+public SpringTemplateEngine templateEngine(SpringResourceTemplateResolver templateResolver) {
+		SpringTemplateEngine templateEngine=new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver);
+	templateEngine.setEnableSpringELCompiler(true);
+	return templateEngine;}
+
+}
