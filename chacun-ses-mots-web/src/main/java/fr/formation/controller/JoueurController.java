d61e7cddcc75b282a03151e2c78c66a3d6089083
Sylvain Chea <sylvainmb@hotmail.com>
2019-05-03 14:44:38 +0200

    controller 2

--- chacun-ses-mots-web/src/main/java/fr/formation/controller/JoueurController.java
+++ chacun-ses-mots-web/src/main/java/fr/formation/controller/JoueurController.java
@@ -1,47 +1,41 @@
 package fr.formation.controller;
 
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import fr.formation.model.Joueur;
-import fr.formation.projet.IDAOJoueur;
 
 @Controller
 public class JoueurController {
 
-	@Autowired
-	private IDAOJoueur daoJoueur;
-	
-	@GetMapping({"/connexion"})
+//	@Autowired
+//	private IDAOJoueur daoJoueur;
+
+	@GetMapping({ "/connexion" })
 	public String connexion(Model model) {
-		
+
 		return "csmConnexion";
 	}
-	
-	@PostMapping({ "/connexion" })
-	public String connexion(@Valid @ModelAttribute Joueur j, BindingResult result, Model model) {
-		if (result.hasErrors()) {
 
-			return "csmConnexion";
-		}
-		
-		daoJoueur.save(j);
-		return "redirect:/csmJoueur";
-	}
-	
-	@GetMapping({"/profil/{idJoueur}"})
+//	@PostMapping({ "/connexion" })
+//	public String connexion(@Valid @ModelAttribute Joueur j, BindingResult result, Model model) {
+//		if (result.hasErrors()) {
+//
+//			return "csmConnexion";
+//		}
+//
+//		if (daoJoueur.findByPseudoAndMotDePasse(j.getPseudo(), j.getMotDePasse()).isPresent()) {
+//			return "redirect:/csmJoueur";
+//		}
+//		model.addAttribute("erreurConnexion", "Pseudo ou mot de passe invalide");
+//		return "csmConnexion";
+//	}
+//
+	@GetMapping({ "/profil/{idJoueur}" })
 	public String profil(@PathVariable int idJoueur, Model model) {
-		
+
 		return "csmProfil";
-		
+
 	}
 
 }
--- chacun-ses-mots-web/src/main/java/fr/formation/controller/MenuController.java
+++ chacun-ses-mots-web/src/main/java/fr/formation/controller/MenuController.java
@@ -1,37 +1,32 @@
 package fr.formation.controller;
 
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import fr.formation.model.Partie;
 import fr.formation.projet.IDAOPartie;
 
 @Controller
 public class MenuController {
-	
-	@Autowired
-	private IDAOPartie daoPartie;
+//	
+//	@Autowired
+//	private IDAOPartie daoPartie;
 	
 	@GetMapping({"/menu"})
 	public String menu(Model model) {
 		return "csmMenu";
 	}
 	
-	@PostMapping({"/menu"})
-	public String menu(@Valid @ModelAttribute Partie p, BindingResult result, Model model) {
-		if (result.hasErrors()) {
-			
-			return "csmMenu";
-		}
-		daoPartie.save(p);
-		return "redirect:/setgame";
-	}
+//	@PostMapping({"/menu"})
+//	public String menu(@Valid @ModelAttribute Partie p, BindingResult result, Model model) {
+//		if (result.hasErrors()) {
+//			
+//			return "csmMenu";
+//		}
+//		daoPartie.save(p);
+//		return "redirect:/setgame";
+//	}
 
 }
