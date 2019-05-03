a599945b45f260ff7cb3dde3e4b01fa53d9471d8
Sylvain Chea <sylvainmb@hotmail.com>
2019-05-03 15:20:49 +0200

    jpa fix

--- chacun-ses-mots-web/src/main/java/fr/formation/controller/MenuController.java
+++ chacun-ses-mots-web/src/main/java/fr/formation/controller/MenuController.java
@@ -1,11 +1,17 @@
 package fr.formation.controller;
 
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import fr.formation.model.Partie;
 import fr.formation.projet.IDAOPartie;
 
 @Controller
@@ -19,14 +25,14 @@ public class MenuController {
 		return "csmMenu";
 	}
 	
-//	@PostMapping({"/menu"})
-//	public String menu(@Valid @ModelAttribute Partie p, BindingResult result, Model model) {
-//		if (result.hasErrors()) {
-//			
-//			return "csmMenu";
-//		}
-//		daoPartie.save(p);
-//		return "redirect:/setgame";
-//	}
+	@PostMapping({"/menu"})
+	public String menu(@Valid @ModelAttribute Partie p, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			
+			return "csmMenu";
+		}
+		daoPartie.save(p);
+		return "redirect:/setgame";
+	}
 
 }
